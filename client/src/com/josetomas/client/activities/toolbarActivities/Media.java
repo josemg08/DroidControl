d7d0e3588f6a35fe024c7e05672d7dfc4594dce4
Jose Gonzalez <josemg08@gmail.com>
2013-12-17 11:58:11 -0300

    Green skin.

--- client/src/com/josetomas/client/activities/toolbarActivities/Media.java
+++ client/src/com/josetomas/client/activities/toolbarActivities/Media.java
@@ -2,14 +2,15 @@ package com.josetomas.client.activities.toolbarActivities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import com.josetomas.client.R;
 import com.josetomas.client.xmlMessage.keyboardMessage.ShortCutMessage;
 
 public class Media extends ToolbarActivities {
-
     public static final String PLAY = "PLAY";
     public static final String FORWARD = "FORWARD";
     public static final String BACK = "BACK";
+    private boolean play = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +28,33 @@ public class Media extends ToolbarActivities {
     }
 
     public void mediaPlayPause(View view) {
-        ShortCutMessage shortCutSms = new ShortCutMessage(PLAY);
-        socketService.sendMessage(parseXML.buildXMLMessage(shortCutSms));
+        if(!play){
+            ShortCutMessage shortCutSms = new ShortCutMessage(PLAY);
+            socketService.sendMessage(parseXML.buildXMLMessage(shortCutSms));
+            view.setBackgroundResource(R.drawable.black_pause_selector);
+
+            if(preferences.getString("colorApp", "Black").equals("Black")){
+                view.setBackgroundResource(R.drawable.black_pause_selector);
+            }
+            else{
+                view.setBackgroundResource(R.drawable.red_pause_selector);
+            }
+
+            play = true;
+        }
+        else{
+            ShortCutMessage shortCutSms = new ShortCutMessage(PLAY);
+            socketService.sendMessage(parseXML.buildXMLMessage(shortCutSms));
+
+            if(preferences.getString("colorApp", "Black").equals("Black")){
+                view.setBackgroundResource(R.drawable.black_arrow_rigth_square_selector);
+            }
+            else{
+                view.setBackgroundResource(R.drawable.red_arrow_rigth_square_selector);
+            }
+
+            play = false;
+        }
     }
 
     public void mediaForward(View view){
@@ -41,7 +67,7 @@ public class Media extends ToolbarActivities {
         socketService.sendMessage(parseXML.buildXMLMessage(shortCutSms));
     }
 
-    private void fullScreen(View view) {
+    public void fullScreen(View view) {
         ShortCutMessage shortCutSms = new ShortCutMessage("FULLSCREEN");
         socketService.sendMessage(parseXML.buildXMLMessage(shortCutSms));
     }
